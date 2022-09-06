f33d550c1bc6a71751b56ff7eb8a3d06f585e9a9
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 22:42:39 +0530

    Null String ExceptionHandling

---
+++ MoodAnalyser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string message = "I am in Sad Mood";
+        private string testmessage = "";
+       
+        public MoodAnalyser()
+        {
+
+        }
+        public MoodAnalyser(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+
+            try
+
+            {
+                if (this.testmessage == "NULL" || this.testmessage == string.Empty)
+                {
+                    throw new Exception("Mood is Null");
+                }
+
+                if (this.testmessage == message)
+                {
+                    return "SAD";
+                }
+                else if (this.testmessage == "I am in Happy Mood")
+                {
+                    return "Happy";
+                }
+                else
+                {
+                    throw new Exception("Mood is Null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Happy";
+            }
+        }
+    }
+}
