7debf4eace9bfe7d5ab4d29c181773a43feac9c9
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 23:05:57 +0530

    Reflection to Invoke Method

---
+++ UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moodanalyzer;
+
+namespace MoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AnalyseMood()
+        {
+            //Arrange
+            MoodAnalyser moodAnalyzer = new MoodAnalyser();
+            string meassage = "I am in Sad Mood";
+            string ExpectedOutput = "SAD";
+
+            string result = moodAnalyzer.AnalyseMood();
+
+            Assert.AreEqual(result, ExpectedOutput);
+
+        }
+    }
+}
