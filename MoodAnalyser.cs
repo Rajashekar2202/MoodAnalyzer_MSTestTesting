3fe6a0b285cc45b54bce168e3f897758b9c20d62
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 22:33:15 +0530

    Handle mood Sad or Happy

---
+++ MoodAnalyser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string message = "I am in Sad Mood";
+        private string testmessage = "";
+       
+        public MoodAnalyser()
+        {
+
+        }
+        public MoodAnalyser(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.testmessage == message)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
